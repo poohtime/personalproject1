5199b0c50e8586880382fade6ae91427e8ff3fd6
poohtime <pooh_time@naver.com>
2023-10-26 10:28:11 +0900

    스크립트 분리

--- js/scripts.js
+++ js/scripts.js
@@ -70,7 +70,6 @@ const init = async() => {
 }
 
 
-
 init();
 
 document.getElementById("navbar-fixed").innerHTML = `
@@ -90,7 +89,7 @@ document.getElementById("navbar-fixed").innerHTML = `
       <div class="collapse navbar-collapse" id="navbarExample01">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <a id = "mpbtn" class="mainbtn" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Features</a>
